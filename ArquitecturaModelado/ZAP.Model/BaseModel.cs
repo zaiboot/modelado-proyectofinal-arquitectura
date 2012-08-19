f1ad71bffcc0e03cc936c565a0176845a16421bb
zaiboot@gmail.com <zaiboot@gmail.com@762bdeb3-92e6-7fbb-ab85-b58175fdfb53>
2012-08-19 01:14:21 +0000

    Adding model and facade layers.

---
+++ ArquitecturaModelado/ZAP.Model/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZAP.Model
+{
+    /// <summary>
+    /// Base model for all the classes
+    /// </summary>
+    public abstract class BaseModel
+    {
+
+    }
+}
