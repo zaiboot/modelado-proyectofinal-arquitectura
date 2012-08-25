84e11dbcb5ff972a87d91b8c614c4de5aa7aaaec
zaiboot@gmail.com <zaiboot@gmail.com@762bdeb3-92e6-7fbb-ab85-b58175fdfb53>
2012-08-25 23:14:30 +0000

    Adding datasources for the new models

---
+++ trunk/ArquitecturaModelado/ZAP.Model/AuthenticateResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZAP.Model
+{
+    public class AuthenticateResult : BaseModel
+    {
+        public bool IsLoginFine{ get; set; }
+    }
+}
---
+++ trunk/ArquitecturaModelado/ZAP.Model/OperationResutl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZAP.Model
+{
+    /// <summary>
+    /// To set a standart for all the layers for handling errors.
+    /// </summary>
+    public class OperationResult<T> where  T : BaseModel 
+    {
+        /// <summary>
+        /// Define if the request was successfull.
+        /// </summary>
+        public bool IsSuccesed;
+        public string ErrorMessage;
+        public T Data { get; set; }
+ 
+    }
+}
